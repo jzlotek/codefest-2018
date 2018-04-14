df879d67040fee400033290aaca4cecab4dd8e7e
Unknown <djw89@drexel.edu>
2018-04-14 16:58:24 -0400

    Given Address, coords work. Given IP no work

---
+++ web/python/latlng.py
@@ -0,0 +1,13 @@
+import requests
+url = 'https://maps.googleapis.com/maps/api/geocode/json'
+location = "3300 Lancaster Avenue Philadelphia, Pennsylvania"
+params = {'sensor': 'false', 'address': location}
+r = requests.get(url, params=params)
+results = r.json()['results']
+location = results[0]['geometry']['location']
+print(location['lat'])
+print(location['lng'])
+
+import geocoder
+g = geocoder.ipinfo('me')
+print(g.latlng)
